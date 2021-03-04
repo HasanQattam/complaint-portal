d6c5843d8d316c532207640afe2738bd36b11baa
Hasan <hassan.qattam@hotmail.com>
2021-03-04 19:15:39 +0200

    initialize the spring boot project and added the required dependencies

---
+++ src/test/java/com/pwc/complaint_portal/ComplaintPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwc.complaint_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
